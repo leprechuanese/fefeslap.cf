3113f66d437e0371149e4e6fe4a94e039788a4d6
Polsaker <bouramiro@gmail.com>
2014-09-30 14:54:48 -0300

    Putting the ads in the bottom again and fixing a possible vulnerability to mysql injection

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once "includes/functions.php";
 $mysql = connectmysql();
 
 
-$fefe = $_SERVER['QUERY_STRING'];
+$fefe = mysql_escape_string($_SERVER['QUERY_STRING']);
 
 if(is_numeric($fefe)){
     $factoq = getfacto($fefe, $mysql);
@@ -33,7 +33,7 @@ if($factoq == false){
     $title = "fefeslaps with {$url} {$facto}";
 }
 printheader($title);
-adsense();
 printbody($factoq, $mysql);
+adsense();
 disqus();
 printfooter();
