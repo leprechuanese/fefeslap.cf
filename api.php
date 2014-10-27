14901fedbff167dadf1504771d57531f5ee23c03
Polsaker <bouramiro@gmail.com>
2014-10-27 15:26:20 -0300

    Permitiendo borrar fefes desde la API

--- api.php
+++ api.php
@@ -1,4 +1,18 @@
 <?php
+/* API PARA FEFESLAPS
+ * Uso:
+ *   Añadir fefe: /api.php?a=add&user=USUARIO&pass=CONTRASEÑA&fefe=TEXTO_DEL_FEFE
+ *   Eliminar fefe: /api.php?a=del&user=USUARIO&pass=CONTRASEÑA&id=ID_DEL_FEFE
+ *   Ver fefe: /api.php?a=get&id=ID_DEL_FEFE
+ * Retorna:
+ * - >:O si faltan parámetros
+ * - >:( si falta el parámetro "a"
+ * - No. si el usuario o contraseña son incorrectos
+ * - OK si añadió el fefe correctamente
+ * - El fefe cuando a=get finalizó correctamente
+ * - 404 cuando no se ha encontrado el fefe
+ */ 
+
 require("includes/config.php");
 require("includes/functions.php");
 require("includes/error_display.php");
@@ -19,6 +33,24 @@ switch($_GET['a']){
         $sql = "INSERT INTO `{$sql_database}`.`{$sql_table}` (`fefefact`, `created`) VALUES ({$_GET['fefe']}, '".date("Y-m-d")."')";
         $result = $mysql->query($sql);
         echo "OK";
+        break;
+    case "del":
+    case "add":
+        if(!isset($_GET['user']) || !isset($_GET['pass']) || !isset($_GET['id'])){die(">:O");}
+        if(!isset($users[$_GET['user']])){
+            die("No.");
+        }elseif($users[$_GET['user']] != $_GET['pass']){
+            die("No.");
+        }
+        $mysql = connectmysql();
+        $_GET['id'] = $mysql->quote($_GET['id']);
+        $sql = "DELETE FROM `{$sql_database}`.`{$sql_table}` WHERE fefefactID={$_GET['id']}";
+        $result = $mysql->query($sql);
+        if($result->rowCount() != 0){
+            echo "OK";
+        }else{
+            echo "404";
+        }
         break;
     case "get":
         if(!isset($_GET['id'])){die(">:O");}
